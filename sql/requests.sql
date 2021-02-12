6f89c117905e713d05605ea73f3c23bce8fd0708
Maxime Vincent <maxime.vincent1@u-psud.fr>
2021-02-12 12:26:39 +0100

    Dernières requêtes SQL

--- sql/requests.sql
+++ sql/requests.sql
@@ -1,4 +1,4 @@
-PROMPT Noms recommandés protéines commentaires 'cardiac';
+PROMPT REQUEST 1: Noms recommandés protéines commentaires 'cardiac';
 SELECT prot_name_2_prot.accession, protein_names.prot_name
 FROM protein_names, prot_name_2_prot, comments
 WHERE
@@ -8,7 +8,7 @@ WHERE
   AND prot_name_2_prot.prot_name_id = protein_names.prot_name_id
   AND protein_names.name_kind = 'recommendedName';
 
-PROMPT Noms recommandés protéines mots-clé 'Long QT syndrome';
+PROMPT REQUEST 2: Noms recommandés protéines mots-clé 'Long QT syndrome';
 SELECT prot_name_2_prot.accession, protein_names.prot_name 
 FROM protein_names, prot_name_2_prot, keywords, entries_2_keywords
 WHERE 
@@ -19,7 +19,7 @@ WHERE
   AND prot_name_2_prot.prot_name_id = protein_names.prot_name_id
   AND protein_names.name_kind = 'recommendedName';
 
-PROMPT Entrées dont séquence la plus grande;
+PROMPT REQUEST 3: Entrées dont séquence la plus grande;
 SELECT entries.*
 FROM entries, proteins 
 WHERE 
@@ -27,8 +27,50 @@ WHERE
   = (SELECT MAX(seqLength) FROM proteins)
   AND proteins.accession = entries.accession;
 
-PROMPT Entrées plus de 2 gènes;
+PROMPT REQUEST 4: Entrées plus de 2 gènes;
 SELECT accession, COUNT(gene_name_id)
 FROM entry_2_gene_name
 GROUP BY accession
 HAVING COUNT(gene_name_id) > 2;
+
+PROMPT REQUEST 5: Entrées dont nom protéine 'channel';
+SELECT 
+  prot_name_2_prot.accession,
+  protein_names.prot_name,
+  protein_names.name_kind
+FROM protein_names, prot_name_2_prot
+WHERE
+  -- REGEXP_LIKE (prot_name, 'channel', 'i')
+  prot_name LIKE '%channel%'
+  AND protein_names.prot_name_id = prot_name_2_prot.prot_name_id;
+
+PROMPT REQUEST 6: Entrées dont nom protéine 'Long QT syndrome' et 'Short QT syndrome';
+SELECT DISTINCT
+  prot_name_2_prot.accession,
+  protein_names.prot_name
+FROM 
+  keywords, entries_2_keywords,
+  protein_names, prot_name_2_prot
+WHERE
+  -- (REGEXP_LIKE (keywords.kw_label, 'Long QT syndrome', 'i')
+  -- OR REGEXP_LIKE (keywords.kw_label, 'Short QT syndrome', 'i'))
+  (keywords.kw_label LIKE '%Long QT syndrome%'
+  OR keywords.kw_label LIKE '%Short QT syndrome%')
+  AND keywords.kw_id = entries_2_keywords.kw_id
+  AND entries_2_keywords.accession = prot_name_2_prot.accession
+  AND prot_name_2_prot.prot_name_id = protein_names.prot_name_id
+  AND protein_names.name_kind = 'recommendedName';
+-- Autre possibilité on enlève le DISTINCT et puis
+-- GROUP BY (prot_name_2_prot.accession, protein_names.prot_name)
+
+PROMPT REQUEST 7: Termes GO 'Long QT syndrome' communs 2 entrées;
+SELECT dbref.db_ref
+FROM dbref, keywords, entries_2_keywords
+WHERE
+  -- REGEXP_LIKE (keywords.kw_label, 'Long QT syndrome', 'i')
+  keywords.kw_label LIKE '%Long QT syndrome%'
+  AND keywords.kw_id = entries_2_keywords.kw_id
+  AND entries_2_keywords.accession = dbref.accession
+  AND dbref.db_type = 'GO'
+GROUP BY dbref.db_ref
+HAVING COUNT(dbref.accession) >= 2;
