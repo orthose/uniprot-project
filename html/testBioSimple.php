66001348815249079b21b54aafa36baaf2acb0a2
Maxime Vincent <maxime.vincent1@u-psud.fr>
2021-02-13 11:18:00 +0100

    Ré-indentation des fihciers html

--- html/testBioSimple.php
+++ html/testBioSimple.php
@@ -1,39 +1,33 @@
 <html>
+  <head> 
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    Un exemple pour le projet PHP 
+  </head>
+  <body>
+    <?php
+    // Récupérer dans des variables locales les paramètres du formulaire
+    $ac = $_REQUEST['accession'];
 
-    <head> 
-        <link rel="stylesheet" href="style.css" type="text/css" />
-        Un exemple pour le projet PHP 
-    </head>
+    $connexion = oci_connect('c##pandrie_a', 'pandrie_a', 'dbinfo');
+    $txtReq = " select dateCreat, dataset "
+      ."from entries e "."where e.accession = :acces ";
+    // Pour débugger on affiche le texte de la requête:
+    // echo "<i>(debug : ".$txtReq.")</i><br>";
 
-    <body>
+    $ordre = oci_parse($connexion, $txtReq);
 
-        <?php
+    oci_bind_by_name($ordre, ":acces", $ac);
 
-            // Récupérer dans des variables locales les paramètres du formulaire
-            $ac = $_REQUEST['accession'];
+    // Exécution de la requête
+    oci_execute($ordre);
 
-            $connexion = oci_connect('c##pandrie_a', 'pandrie_a', 'dbinfo');
-            $txtReq = " select dateCreat, dataset "
-            . "from entries e "
-            . "where e.accession = :acces ";
-            // Pour débugger on affiche le texte de la requête:
-            // echo "<i>(debug : ".$txtReq.")</i><br>";
+    while (($row = oci_fetch_array($ordre, OCI_BOTH)) !=false) {
+      echo '<br> ' . $row[0] . ' ' . $row[1] ; 
+    }
 
-            $ordre = oci_parse($connexion, $txtReq);
+    oci_free_statement($ordre);
+    oci_close($connexion);
 
-            oci_bind_by_name($ordre, ":acces", $ac);
-
-            // Exécution de la requête
-            oci_execute($ordre);
-
-            while (($row = oci_fetch_array($ordre, OCI_BOTH)) !=false) {
-                echo '<br> ' . $row[0] . ' ' . $row[1] ; 
-            }
-
-            oci_free_statement($ordre);
-            oci_close($connexion);
-
-        ?>
-
-    </body>
+    ?>
+  </body>
 </html>
